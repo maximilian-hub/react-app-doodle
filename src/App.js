0e6068cc4121961c4b2994113babbbaf6cb90130
maximilian-hub <maximilian.spedale@gmail.com>
2022-12-12 18:03:15 -0600

    refactored. taking the form of a todo list app.  added components: Form, Todo, FilterButton.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import "./App.css";
 import React, { useState } from "react";
-import data from "./data.json";
 
-import Header from "./Header";
-import TaskList from "./TaskList";
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 
-function App() {
-  const [taskList, setTaskList] = useState(data);
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id} //anything rendered with iteration should be passed a unique key.
+    />
+  ));
+
+  function addTask(name) {
+    const newTask = { id: "id", name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  console.log("foo");
 
   return (
-    <div className="App">
-      <Header />
-      <TaskList taskList={taskList} />
+    <div className="todoapp stack-large">
+      <h1>ToDoMaster 5000</h1>
+
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">3 tasks remaining:</h2>
+      <ul
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
--- src/Task.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Task = ({ task }) => {
-  return <div>{task.description}</div>;
-};
-
-export default Task;
