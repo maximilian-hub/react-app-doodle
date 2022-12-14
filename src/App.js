13b8d6d7233bd994034cbaf225d24a8dafcfc2db
maximilian-hub <maximilian.spedale@gmail.com>
2022-12-14 15:39:15 -0600

    implemented adding/removing tasks. implemented editing tasks.

--- src/App.js
+++ src/App.js
@@ -4,36 +4,82 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
 
   const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
+      isEditing={false}
       key={task.id} //anything rendered with iteration should be passed a unique key.
+      toggleTaskCompleted={toggleTaskCompleted} //callback prop, allows child component to send data back up
+      deleteTask={deleteTask} //callback prop
+      editTask={editTask} //callback prop
     />
   ));
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining:`;
+
+  const filterList = FILTER_NAMES.map((name) => {
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />;
+  });
 
   function addTask(name) {
     const newTask = { id: "id", name, completed: false };
     setTasks([...tasks, newTask]);
   }
 
-  console.log("foo");
+  function deleteTask(id) {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+
+    setTasks(updatedTasks);
+  }
+
+  function editTask(id, newName) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+
+    setTasks(updatedTasks);
+  }
 
   return (
     <div className="todoapp stack-large">
       <h1>ToDoMaster 5000</h1>
 
       <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
-      <h2 id="list-heading">3 tasks remaining:</h2>
+      <div className="filters btn-group stack-exception">{filterList}</div>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
