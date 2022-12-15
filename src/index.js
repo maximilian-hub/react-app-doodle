858bc99190c4c551e9add7d0ab26b6125c6c024b
maximilian-hub <maximilian.spedale@gmail.com>
2022-12-15 13:21:38 -0600

    removed unnecessary file. changed mock data for fun.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import "./index.css";
 import App from "./App";
 
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false },
+  { id: "todo-0", name: "learn valuable skills", completed: true },
+  { id: "todo-1", name: "build projects", completed: true },
+  { id: "todo-2", name: "get a job", completed: false },
+  {
+    id: "todo-3",
+    name: "swear undying fealty to your new employer",
+    completed: false,
+  },
 ];
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
